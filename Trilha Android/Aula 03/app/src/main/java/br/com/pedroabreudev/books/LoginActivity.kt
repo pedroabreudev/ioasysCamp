afdd57fa3da61290e8f4fb880187861fd9ef7036
Pedro Henrique <pedroabreu.dev@gmail.com>
2022-01-23 16:39:52 -0400

    Finalizando Aula 03

--- Trilha Android/Aula 03/app/src/main/java/br/com/pedroabreudev/books/LoginActivity.kt
+++ Trilha Android/Aula 03/app/src/main/java/br/com/pedroabreudev/books/LoginActivity.kt
@@ -2,10 +2,21 @@ package br.com.pedroabreudev.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
+import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val txtError = findViewById<AppCompatTextView>(R.id.textError)
+        val button = findViewById<MaterialButton>(R.id.enterButton)
+
+        button.setOnClickListener {
+            txtError.visibility = View.VISIBLE
+
+        }
     }
 }
