0a8abbab3e7a4c8461cb01647093dda13f59445b
Pedro Henrique <pedroabreu.dev@gmail.com>
2022-01-25 22:53:02 -0400

    Created Icons, Background Books, Custom View

--- Trilha Android/Aula 03/app/src/main/java/br/com/pedroabreudev/books/LoginActivity.kt
+++ Trilha Android/Aula 03/app/src/main/java/br/com/pedroabreudev/books/LoginActivity.kt
@@ -1,22 +1,29 @@
 package br.com.pedroabreudev.books
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatTextView
 import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
+
+    private val txtError: AppCompatTextView by lazy {
+        findViewById(R.id.textError)
+    }
+
+    private val enterButton: MaterialButton by lazy {
+        findViewById(R.id.enterButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val txtError = findViewById<AppCompatTextView>(R.id.textError)
-        val button = findViewById<MaterialButton>(R.id.enterButton)
-
-        button.setOnClickListener {
-            txtError.visibility = View.VISIBLE
-
+        enterButton.setOnClickListener {
+            val intent = Intent(this, BookListActivity::class.java)
+            startActivity(intent)
         }
     }
 }
