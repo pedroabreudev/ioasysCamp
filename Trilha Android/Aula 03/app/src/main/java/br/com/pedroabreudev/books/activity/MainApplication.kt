e85653fe17ddc3a2625766f70325f5562acb7aa3
Pedro Henrique <pedroabreu.dev@gmail.com>
2022-02-21 08:38:08 -0400

    Adicionando Splash Logo Ioasys Theme.

--- Trilha Android/Aula 03/app/src/main/java/br/com/pedroabreudev/books/activity/MainApplication.kt
+++ Trilha Android/Aula 03/app/src/main/java/br/com/pedroabreudev/books/activity/MainApplication.kt
@@ -1,6 +1,7 @@
 package br.com.pedroabreudev.books.activity
 
 import android.app.Application
+import br.com.pedroabreudev.books.R
 import br.com.pedroabreudev.books.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -9,6 +10,7 @@ class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        setTheme(R.style.Theme_Books)
         startKoin {
             modules(
                 presentationModule,
